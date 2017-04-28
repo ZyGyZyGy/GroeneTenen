42394086f3d810a423a41cd6dccb44ec31f0435e
Alex <jegorov.aleksandr@yahoo.com>
2017-04-28 16:55:49 +0200

    12.8 Een POST request - Eenvoudige views met <mvc:view-controller>

--- src/main/java/be/vdab/web/FiliaalController.java
+++ src/main/java/be/vdab/web/FiliaalController.java
@@ -1,15 +1,20 @@
 package be.vdab.web;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/filialen")
 class FiliaalController {
-    
+
     private static final String FILIALEN_VIEW = "filialen/filialen";
     private static final String TOEVOEGEN_VIEW = "filialen/toevoegen";
+    private static final String REDIRECT_URL_NA_TOEVOEGEN = "redirect:/filialen";
+    private static final Logger LOGGER = Logger.getLogger(FiliaalController.class.getName());
 
     @GetMapping
     String findAll() {
@@ -20,5 +25,12 @@ class FiliaalController {
     String createForm() {
 	return TOEVOEGEN_VIEW;
     }
-    
+
+    @PostMapping
+    String create() {
+	// later voeg je een record toe aan de database
+	LOGGER.info("filiaal record toevoegen aan database");
+	return REDIRECT_URL_NA_TOEVOEGEN;
+    }
+
 }
