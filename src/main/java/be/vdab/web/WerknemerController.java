10827453a1adacf56b8d9fdf7b53385d151b5554
Alex <jegorov.aleksandr@yahoo.com>
2017-05-09 12:54:58 +0200

    30.9 OpenEntityManagerInViewFilter

--- src/main/java/be/vdab/web/WerknemerController.java
+++ src/main/java/be/vdab/web/WerknemerController.java
@@ -3,16 +3,25 @@ package be.vdab.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import be.vdab.services.WerknemerService;
 
 @Controller
 @RequestMapping("/werknemers")
 class WerknemerController {
-    
+
     private static final String WERKNEMERS_VIEW = "werknemers/werknemers";
 
-    @GetMapping
-    String findAll() {
-	return WERKNEMERS_VIEW;
+    private final WerknemerService werknemerService;
+
+    WerknemerController(WerknemerService werknemerService) {
+	this.werknemerService = werknemerService;
     }
-    
+
+    @GetMapping
+    ModelAndView findAll() {
+	return new ModelAndView(WERKNEMERS_VIEW, "werknemers", werknemerService.findAll());
+    }
+
 }
