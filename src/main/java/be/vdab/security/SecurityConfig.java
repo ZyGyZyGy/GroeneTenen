aa6d4829bc429a56ba64fcfd1063639a3c01806f
Aleks <Aleks@LOK111-05>
2017-05-18 13:39:04 +0200

    41.9.1 Een afwijkende tabelstructuur voor principals en authorization

--- src/main/java/be/vdab/security/SecurityConfig.java
+++ src/main/java/be/vdab/security/SecurityConfig.java
@@ -19,6 +19,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
     private static final String MAGAZIJNIER = "magazijnier";
     
+    private static final String USERS_BY_USERNAME = 
+	    "select naam as username, paswoord as password, actief as enabled " +  
+	    "from gebruikers where naam = ?";
+    private static final String AUTHORITIES_BY_USERNAME = 
+	    "select gebruikers.naam as username, rollen.naam as authorities " + 
+	    "from gebruikers inner join gebruikersrollen " +  
+	    "on gebruikers.id = gebruikersrollen.gebruikerid " + 
+	    "inner join rollen " + 
+	    "on rollen.id = gebruikersrollen.rolid " + 
+	    "where gebruikers.naam= ?"; 
+    
     private final DataSource dataSource;
 
     public SecurityConfig(DataSource dataSource) {
@@ -27,7 +38,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-	auth.jdbcAuthentication().dataSource(dataSource);
+	auth.jdbcAuthentication().dataSource(dataSource)
+		.usersByUsernameQuery(USERS_BY_USERNAME)
+		.authoritiesByUsernameQuery(AUTHORITIES_BY_USERNAME);
     }
 
     @Override
@@ -41,14 +54,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 	http.formLogin().loginPage("/login")
-				.and().logout().logoutSuccessUrl("/")
-                		.and().authorizeRequests()
-                		.antMatchers("/filialen/toevoegen", "/filialen/*/wijzigen", "/filialen/*/verwijderen").hasAnyAuthority(MANAGER, CEO)
-                		.antMatchers(HttpMethod.POST, "/filialen").hasAnyAuthority(MANAGER, CEO)
-                		.antMatchers("/werknemers").hasAnyAuthority(MAGAZIJNIER, HELPDESKMEDEWERKER, CEO)
-                		.antMatchers("/", "/login").permitAll()  
-                		.antMatchers("/**").authenticated()
-                		.and().exceptionHandling().accessDeniedPage("/WEB-INF/JSP/forbidden.jsp");
+		.and().logout().logoutSuccessUrl("/")
+    		.and().authorizeRequests()
+    		.antMatchers("/filialen/toevoegen", "/filialen/*/wijzigen", "/filialen/*/verwijderen").hasAnyAuthority(MANAGER, CEO)
+    		.antMatchers(HttpMethod.POST, "/filialen").hasAnyAuthority(MANAGER, CEO)
+    		.antMatchers("/werknemers").hasAnyAuthority(MAGAZIJNIER, HELPDESKMEDEWERKER, CEO)
+    		.antMatchers("/", "/login").permitAll()  
+    		.antMatchers("/**").authenticated()
+    		.and().exceptionHandling().accessDeniedPage("/WEB-INF/JSP/forbidden.jsp");
     }
     
     
