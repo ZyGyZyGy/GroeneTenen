6025df1529253bd85d6112eef44cbfc0dff7ffe5
Aleks <Aleks@LOK111-05>
2017-05-18 10:42:42 +0200

    41 SPRING SECURITY

---
+++ src/main/java/be/vdab/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package be.vdab.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    private static final String MANAGER = "manager";
+    private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
+    private static final String MAGAZIJNIER = "magazijnier";
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.inMemoryAuthentication()
+		.withUser("joe").password("theboss").authorities(MANAGER)
+		.and().withUser("averell").password("hungry").authorities(HELPDESKMEDEWERKER, MAGAZIJNIER);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+	web.ignoring()
+		.antMatchers("/images/**")
+		.antMatchers("/styles/**")
+		.antMatchers("/scripts/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+	http.formLogin()
+		.and().authorizeRequests()
+		.antMatchers("/filialen/toevoegen", "/filialen/*/wijzigen", "/filialen/*/verwijderen")
+		.hasAuthority(MANAGER).antMatchers(HttpMethod.POST, "/filialen").hasAuthority(MANAGER)
+		.antMatchers("/werknemers").hasAnyAuthority(MAGAZIJNIER, HELPDESKMEDEWERKER)
+		.antMatchers("/", "/login").permitAll()  
+		.antMatchers("/**").authenticated().
+		and().exceptionHandling().accessDeniedPage("/WEB-INF/JSP/forbidden.jsp");
+    }
+    
+}
